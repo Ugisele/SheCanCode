b5cd5f4396f8dbf017bdba5f870ab504ab97ef74
Ugisele <Umutonigisele12@gmail.com>
2024-01-22 16:06:43 +0200

    Pagination styles

---
+++ responsiveness/script.js
@@ -0,0 +1,8 @@
+function menu_exit() {
+    document.getElementsByClassName("show-hide").style.display = "absolute";
+}
+function menu_open() {
+    document.getElementsByClassName("show-hide").style.display =
+        "none";
+
+}
